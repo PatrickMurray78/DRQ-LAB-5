f18fa42c12aba2ab9512992645c9f7618f524623
patrickmurray73 <g00344530@gmit.ie>
2020-11-03 15:30:11 +0000

    Added Post method

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Welcome to Data Representation & Querying!');
@@ -34,6 +41,14 @@ app.get('/api/movies', (req, res) => {
 
 app.get('/test', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/name', (req, res) => {
+    res.send('Hello ' + req.query.fname + ' ' + req.query.lname);
+});
+
+app.post('/name', (req, res) => {
+    res.send('Hello ' + req.body.fname + ' ' + req.body.lname);
 });
 
 app.listen(port, () => {
